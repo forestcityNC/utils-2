637e3115703d83575f50ec06dfdffe9d5d1da7a9
dbharris92 <harris.db92@gmail.com>
2020-05-14 09:21:29 -0400

    Update cartoDBProxy.php

--- php/cartoDBProxy.php
+++ php/cartoDBProxy.php
@@ -4,9 +4,9 @@ $cache_limiter = session_cache_limiter();
 function goProxy($dataURL)
 {
 	$baseURL = 'http://dbharris92tofc.cartodb.com/api/v2/sql?';
-	//  					^ CHANGE THE 'CARTODB-USER-NAME' to your cartoDB url!
+	//  					^ cartoDB url
 	$api = '&api_key=3c54e9eb73aedf7d17ca4bf6915a7b8ccf02c6a5';
-	//				 ^ENTER YOUR API KEY HERE!
+	//				 ^api key
 	$url = $baseURL.'q='.urlencode($dataURL).$api;
 	$result = file_get_contents ($url);
 	return $result;
