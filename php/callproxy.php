b25e0127bebdb93c357a5c99524cdd2be432bef6
dbharris92 <harris.db92@gmail.com>
2020-05-14 09:36:34 -0400

    Update callproxy.php

--- php/callproxy.php
+++ php/callproxy.php
@@ -1,5 +1,5 @@
 <?php
-include 'cartoDBProxy.php';
+include './php/cartoDBProxy.php';
 //			^CHANGE THIS TO THE PATH TO YOUR cartodbProxy.php
 $queryURL = $_POST['qurl'];
 $return = goProxy($queryURL);
